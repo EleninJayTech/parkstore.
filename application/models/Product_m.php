e1c187aacda090063407d2439b37093faeb36d0d
bsjeong <eleninjaytech@gmail.com>
2021-04-17 19:48:33 +0900

    초이템 저장 1차 완료

--- application/models/Product_m.php
+++ application/models/Product_m.php
@@ -57,4 +57,68 @@ class Product_m extends MY_Model {
 		]);
 		return $this->db->insert_batch('product_info_list', $saveData);
 	}
+
+	/**
+	 * 상품 정보 기록
+	 * @param $saveData
+	 * @return bool|int
+	 */
+	public function saveOptionInfo($saveData){
+		if( count($saveData) == 0 ){
+			return true;
+		}
+
+		$this->db->delete('product_option_list', [
+			'product_id'=>$saveData[0]['product_id'],
+		]);
+		return $this->db->insert_batch('product_option_list', $saveData);
+	}
+
+	/**
+	 * 상품 기타 정보 기록
+	 * @param $saveData
+	 * @return bool|int
+	 */
+	public function saveEtcInfo($saveData){
+		if( count($saveData) == 0 ){
+			return true;
+		}
+
+		$this->db->delete('product_etc_info', [
+			'product_id'=>$saveData[0]['product_id'],
+		]);
+		return $this->db->insert_batch('product_etc_info', $saveData);
+	}
+
+	/**
+	 * 상품 이미지 정보 기록
+	 * @param $saveData
+	 * @return bool|int
+	 */
+	public function saveImgInfo($saveData){
+		if( count($saveData) == 0 ){
+			return true;
+		}
+
+		$this->db->delete('product_img', [
+			'product_id'=>$saveData[0]['product_id'],
+		]);
+		return $this->db->insert_batch('product_img', $saveData);
+	}
+
+	/**
+	 * 상품 이미지 상세 정보 기록
+	 * @param $saveData
+	 * @return bool|int
+	 */
+	public function saveImgDetailInfo($saveData){
+		if( count($saveData) == 0 ){
+			return true;
+		}
+
+		$this->db->delete('detail_img', [
+			'product_id'=>$saveData[0]['product_id'],
+		]);
+		return $this->db->insert_batch('detail_img', $saveData);
+	}
 }
